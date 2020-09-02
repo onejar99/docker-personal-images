5e4f59393e2b11b8c9bb246a795dc5ab643e450a
OneJar <onejar99@gmail.com>
2020-09-03 00:04:01 +0800

    Update list of docker pulling

--- scripts/docker-pulling.sh
+++ scripts/docker-pulling.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
+# gitbook
+docker pull onejar99/gitbook:light
+docker pull onejar99/gitbook:light_20200617
+docker pull onejar99/gitbook:full
+docker pull onejar99/gitbook:full_20200617
+
+# mysql
+docker pull onejar99/mysql:5.6
+
+# nodejs-live-server
+docker pull onejar99/nodejs-live-server:node12.16.1
+docker pull onejar99/nodejs-live-server:https_node12.16.1
+docker pull onejar99/nodejs-live-server:https_node12.16.1_20200828
+
+# php-apache
+docker pull onejar99/php-apache:php5.5.29
+docker pull onejar99/php-apache:php5.5.29_20200402
+docker pull onejar99/php-apache:php5.5.29_20200614
+
+# phpmyadmin
+docker pull onejar99/phpmyadmin:latest
+
+# web-demo
 docker pull onejar99/web-demo:v1
 docker pull onejar99/web-demo:v2
